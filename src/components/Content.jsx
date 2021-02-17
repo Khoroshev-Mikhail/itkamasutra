4cd3541f465fb14e26ca95fd9549ea267b4d16f7
Mikha38 <oyks1379@yandex.ru>
2021-02-17 14:36:20 +0300

    первоначальная версия

---
+++ src/components/Content.jsx
@@ -0,0 +1,26 @@
+const Content = () => {
+    return (
+        <div className="content">
+            <div>
+            header
+            </div>
+            <div>
+            ava + descs
+            </div>
+            <div>
+            my posts
+            </div>
+            <div>
+            new post
+            </div>
+            <div>
+            post1
+            </div>
+            <div>
+            post2
+            </div>
+        </div>
+    );
+}
+
+export default Content;
